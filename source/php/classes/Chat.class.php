f77eb00de54afd7b190e4e264de3376c6571a1c3
nussbam <marc.nussbaumer@stud.hslu.ch>
2016-11-06 22:54:57 +0100

    Show users that are in the system

--- source/php/classes/Chat.class.php
+++ source/php/classes/Chat.class.php
@@ -111,7 +111,6 @@ class Chat{
 			throw new Exception('You are not logged in');
 		}
 
-		throw new Exception($_SESSION['user']);
 		if(!$chatText){
 			throw new Exception('You haven\' entered a chat message.');
 		}
@@ -141,7 +140,7 @@ class Chat{
 		
 		DB::query("DELETE FROM webchat_lines WHERE ts < SUBTIME(NOW(),'0:5:0')");
 		
-		$result = DB::query("SELECT * FROM webchat_users ORDER BY name ASC LIMIT 18 WHERE status='active'");
+		$result = DB::query("SELECT * FROM webchat_users ORDER BY name ASC LIMIT 18");
 		
 		$users = array();
 		while($user = $result->fetch_object()){
