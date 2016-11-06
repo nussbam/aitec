d7993a47045abd3333a696d7c6dba63935ef56ca
nussbam <marc.nussbaumer@stud.hslu.ch>
2016-11-06 23:12:44 +0100

    check return value dbquery

--- source/php/classes/ChatLine.class.php
+++ source/php/classes/ChatLine.class.php
@@ -25,6 +25,11 @@ class ChatLine extends ChatBase{
 			Select userlevel FROM webchat_lines WHERE user=
 				'".DB::esc($this->author)."'
 		)");
+
+        throw new Exception("
+			Select userlevel FROM webchat_lines WHERE user=
+				'".DB::esc($this->author)."'
+		)");
         $row = mysqli_fetch_assoc($result);
         throw new Exception($row['userlevel']);
 
